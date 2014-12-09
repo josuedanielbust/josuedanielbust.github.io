53809013c6c6ea0ff61e38eaf0deb35f63cea17c
JosueDanielBust <josuedanielbustamante@gmail.com>
2014-12-09 12:14:07 -0500

    Add mrc page

---
+++ mrc/conf.php
@@ -0,0 +1,8 @@
+<?php
+$conf = array();
+$conf['email_to'] = 'josuedanielbustamante@gmail.com'; // your email address to send notifications to
+
+/* normally not required to edit below this line */
+$conf['email_sent_from'] = $conf['email_to'];
+$conf['email_sent_from_name'] = 'Formulario de Contacto';
+$conf['email_subject'] = 'Formulario de Contacto - MR Construcciones';
---
+++ mrc/views/thanks.html.php
@@ -0,0 +1,3 @@
+<div class="alert alert-success" style="text-align:center;">
+	<span><b>Hemos recibido su mensaje.</b><br>En poco tiempo nos estaremos poniendo en contacto con usted</span>
+</div>
